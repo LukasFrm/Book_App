533d03476df64793b43f716f41b06d60aa1dc674
Lukas Freimonas <lukas.freimonas@manounija.lt>
2020-01-09 09:48:58 +0200

    Final commit (User login still not working)

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,17 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import auth from "./routes/auth";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.post("/api/auth", (req, res) => {
-	res.status(400).json({ errors: { global: "Credentials are invalid" } });
-});
+app.use(bodyParser.json());
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+
+app.use("/api/auth", auth);
 
 app.get("/*", (req, res) => {
 	res.sendFile(path.join(__dirname, "index.html"));
